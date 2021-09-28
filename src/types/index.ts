9a82c65b6f9a88c30cb14547bf4d86182ad5dac1
otoshimtoshi <1083otiket@gmail.com>
2021-09-29 00:42:31 +0900

    削除もれを削除

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,6 @@
 import Router from 'vue-router'
 import { ExStore } from '@/types/store/ex-store-type'
-import { DateUtils } from '@/scripts/utils/date-fns'
 import { ColorModeInstance } from '@nuxtjs/color-mode/types/color-mode'
-import { ScrollUtil } from '@/plugins/scroll-magic'
 
 /**
  * context.app内から使用可能
@@ -11,8 +9,6 @@ declare module '@nuxt/types' {
   interface NuxtAppOptions {
     store: ExStore
     router: Router
-    $dateFns: DateUtils
     $colorMode: ColorModeInstance
-    $scrollmagic: ScrollUtil
   }
 }
