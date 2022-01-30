14fc87debb45901c71aba1c4926a33ef0a698078
otoshimtoshi <1083otiket@gmail.com>
2022-01-30 13:52:54 +0900

    各ページにscopedされたcssをscssに移動

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -30,39 +30,3 @@ useNuxt2Meta(() => ({
   meta: metaInfo?.meta
 }))
 </script>
-
-<style lang="scss" scoped>
-.top {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  pointer-events: none;
-  z-index: 20;
-  animation: fadeIn 3s;
-  .top-inner {
-    width: 100%;
-    height: auto;
-    margin-top: 30px;
-  }
-  .top-title {
-    font-weight: 100;
-    letter-spacing: 0.3em;
-    text-shadow: 2px 5px 5px var(--text-shadow);
-    font-size: 0;
-  }
-  .top-sub-title {
-    letter-spacing: 0.2em;
-    padding: 0 3vw;
-    text-transform: uppercase;
-    font-size: 1vw;
-    @include mq('large', max) {
-      font-size: 14px;
-      padding: 0 2vw;
-    }
-  }
-}
-</style>
