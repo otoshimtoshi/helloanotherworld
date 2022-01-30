2d3a2faf3b9bda9755b4b315b52de6d354d3ccbe
otoshimtoshi <1083otiket@gmail.com>
2022-01-30 14:11:48 +0900

    NavBarの修正

--- src/components/layouts/NavBar.vue
+++ src/components/layouts/NavBar.vue
@@ -1,18 +1,18 @@
 <template>
-  <nav :class="path === 'index' ? '' : 'page'">
-    <ul class="links" :class="path === 'index' ? '' : 'page'">
+  <nav :class="route.name === 'index' ? '' : 'page'">
+    <ul class="links" :class="route.name === 'index' ? '' : 'page'">
       <li
         v-for="(link, index) in links"
         :key="index"
         class="link"
-        :class="path === 'index' ? '' : 'page'"
+        :class="route.name === 'index' ? '' : 'page'"
       >
         <nuxt-link
-          :to="`${link.path}/`"
+          :to="link.path"
           class="link-text"
           :class="[
-            path === 'index' ? '' : 'page',
-            `${link.path}/` === currentRoutePath ? 'line-through' : ''
+            route.name === 'index' ? '' : 'page',
+            link.path === route.path ? 'line-through' : ''
           ]"
         >
           <span>{{ link.text }}</span>
@@ -23,28 +23,24 @@
 </template>
 
 <script setup lang="ts">
-export type Links = {
+type Links = {
   path: string
   text: string
 }
 
-export type PageType = 'index' | 'page'
-
-const { path } = useRoute()
+const route = useRoute()
 const links = reactive<Array<Links>>([
   {
-    path: '/who_i_am',
+    path: '/who_i_am/',
     text: 'Who I Am'
   },
   {
-    path: '/collection',
+    path: '/collection/',
     text: 'Collection'
   },
   {
-    path: '/contact',
+    path: '/contact/',
     text: 'Contact'
   }
 ])
-
-const currentRoutePath = computed(() => path)
 </script>
