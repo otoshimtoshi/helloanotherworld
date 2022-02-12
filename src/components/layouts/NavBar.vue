4d9e3b0108cbad25eea241f2c59281443b70c9d3
otoshimtoshi <1083otiket@gmail.com>
2022-02-12 19:58:03 +0900

    手っ取り早く移転を伝える

--- src/components/layouts/NavBar.vue
+++ src/components/layouts/NavBar.vue
@@ -1,24 +1,8 @@
 <template>
   <nav :class="route.name === 'index' ? '' : 'page'">
-    <ul class="links" :class="route.name === 'index' ? '' : 'page'">
-      <li
-        v-for="(link, index) in links"
-        :key="index"
-        class="link"
-        :class="route.name === 'index' ? '' : 'page'"
-      >
-        <nuxt-link
-          :to="link.path"
-          class="link-text"
-          :class="[
-            route.name === 'index' ? '' : 'page',
-            link.path === route.path ? 'line-through' : ''
-          ]"
-        >
-          <span>{{ link.text }}</span>
-        </nuxt-link>
-      </li>
-    </ul>
+    <a href="https://otot.dev"
+      >2022年2月28日をもってサイトを移転します。https://otot.dev</a
+    >
   </nav>
 </template>
 
@@ -29,18 +13,4 @@ type Links = {
 }
 
 const route = useRoute()
-const links = reactive<Array<Links>>([
-  {
-    path: '/who_i_am/',
-    text: 'Who I Am'
-  },
-  {
-    path: '/collection/',
-    text: 'Collection'
-  },
-  {
-    path: '/contact/',
-    text: 'Contact'
-  }
-])
 </script>
